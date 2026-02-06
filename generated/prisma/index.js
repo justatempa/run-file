c461db5085356b593b40418a8b3fc6ec1e17c8fd
justatempa <3307164933@qq.com>
2026-02-06 18:02:54 +0800

    build

--- generated/prisma/index.js
+++ generated/prisma/index.js
@@ -212,6 +212,7 @@ const config = {
     "db"
   ],
   "activeProvider": "sqlite",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
---
+++ tailwind.config.ts
@@ -0,0 +1,7 @@
+﻿import type { Config } from "tailwindcss";
+
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: { extend: {} },
+  plugins: [],
+} satisfies Config;
